98259d6202f614f16300b6ab9037ebc123b54525
Безобразов Григорий <gabezobrazov@sbercloud.ru>
2023-03-07 19:12:04 +0300

    from last lesson

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+// function Header(props) {
+function Header({ countInHeader, asdasd }) {
+
+  console.log('Header render');
+
+  return (
+    <>
+      <header>Header</header>
+      {asdasd}
+
+      <hr />
+      {countInHeader}
+      <hr />
+    </>
+  )
+}
+
+export { Header }
+
